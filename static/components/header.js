445d33bf32375c14c7d33b2044ee89c4d4318c84
gustKBR <gustavocabralcorreia@gmail.com>
2021-12-26 23:35:48 -0300

    header

---
+++ static/components/header.js
@@ -0,0 +1,17 @@
+function Header (){
+  console.log("oi")
+  document.getElementById('navbar').innerHTML = `
+  <nav>
+    <div class="first-bar"></div>
+    <div class="second-bar"></div>
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="form.html">Formulário</a></li>
+      <li><a href="fluig.html">Fluig</a></li>
+      <li><a href="blog.html">Blog</a></li>
+    </ul>
+  </nav>
+  `
+}
+
+Header()
